78b1896112319093173bdcd9fe14224f486e3a15
arjan bathla <arjanbathla23@gmail.com>
2026-04-01 21:46:18 +0100

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by arjan bathla on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
