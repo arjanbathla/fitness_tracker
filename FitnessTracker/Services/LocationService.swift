f24525a6a0f579001a36358fd9d6a78da5db3f06
arjan bathla <arjanbathla23@gmail.com>
2026-04-04 06:03:49 +0100

    feat: add LocationService with CLLocationManager wrapper

---
+++ FitnessTracker/Services/LocationService.swift
@@ -0,0 +1,34 @@
+import Foundation
+import Combine
+import CoreLocation
+
+class LocationService: NSObject, CLLocationManagerDelegate, ObservableObject {
+    private var locationManager: CLLocationManager
+    @Published var currentLocation: CLLocation?
+
+    override init() {
+        locationManager = CLLocationManager()
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+
+    func stopUpdatingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        currentLocation = location
+        print("location: \(location.coordinate.latitude), \(location.coordinate.longitude)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("location error: \(error.localizedDescription)")
+    }
+}
