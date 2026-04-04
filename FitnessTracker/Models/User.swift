dadb84c0fab6bfcca0104b9227250df64acd9dde
arjan bathla <arjanbathla23@gmail.com>
2026-04-04 06:47:15 +0100

    feat: add Edamam API credentials and macro goal fields to User model

--- FitnessTracker/Models/User.swift
+++ FitnessTracker/Models/User.swift
@@ -1,7 +1,8 @@
 import Foundation
+import FirebaseFirestore
 
 struct User: Identifiable, Codable {
-    var id: String
+    @DocumentID var id: String?
     var fullName: String
     var email: String
     var gender: Gender
@@ -12,6 +13,9 @@ struct User: Identifiable, Codable {
     var stepGoal: Int
     var distanceUnit: DistanceUnit
     var createdAt: Date
+    var proteinGoal: Int
+    var carbsGoal: Int
+    var fatsGoal: Int
 
     enum Gender: String, Codable, CaseIterable {
         case male
@@ -39,8 +43,41 @@ struct User: Identifiable, Codable {
         case miles
     }
 
+    init(fullName: String, email: String, gender: Gender, height: Double, weight: Double, fitnessGoal: FitnessGoal, recommendedCalories: Int, stepGoal: Int, distanceUnit: DistanceUnit, createdAt: Date, proteinGoal: Int? = nil, carbsGoal: Int? = nil, fatsGoal: Int? = nil) {
+        self.fullName = fullName
+        self.email = email
+        self.gender = gender
+        self.height = height
+        self.weight = weight
+        self.fitnessGoal = fitnessGoal
+        self.recommendedCalories = recommendedCalories
+        self.stepGoal = stepGoal
+        self.distanceUnit = distanceUnit
+        self.createdAt = createdAt
+        self.proteinGoal = proteinGoal ?? (recommendedCalories * 30 / 100 / 4)
+        self.carbsGoal = carbsGoal ?? (recommendedCalories * 40 / 100 / 4)
+        self.fatsGoal = fatsGoal ?? (recommendedCalories * 30 / 100 / 9)
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        _id = try container.decode(DocumentID<String>.self, forKey: .id)
+        fullName = try container.decode(String.self, forKey: .fullName)
+        email = try container.decode(String.self, forKey: .email)
+        gender = try container.decode(Gender.self, forKey: .gender)
+        height = try container.decode(Double.self, forKey: .height)
+        weight = try container.decode(Double.self, forKey: .weight)
+        fitnessGoal = try container.decode(FitnessGoal.self, forKey: .fitnessGoal)
+        recommendedCalories = try container.decode(Int.self, forKey: .recommendedCalories)
+        stepGoal = try container.decode(Int.self, forKey: .stepGoal)
+        distanceUnit = try container.decode(DistanceUnit.self, forKey: .distanceUnit)
+        createdAt = try container.decode(Date.self, forKey: .createdAt)
+        proteinGoal = (try? container.decode(Int.self, forKey: .proteinGoal)) ?? (recommendedCalories * 30 / 100 / 4)
+        carbsGoal = (try? container.decode(Int.self, forKey: .carbsGoal)) ?? (recommendedCalories * 40 / 100 / 4)
+        fatsGoal = (try? container.decode(Int.self, forKey: .fatsGoal)) ?? (recommendedCalories * 30 / 100 / 9)
+    }
+
     static let `default` = User(
-        id: "",
         fullName: "",
         email: "",
         gender: .male,
@@ -50,6 +87,9 @@ struct User: Identifiable, Codable {
         recommendedCalories: 2200,
         stepGoal: 10000,
         distanceUnit: .km,
-        createdAt: .now
+        createdAt: .now,
+        proteinGoal: 165,
+        carbsGoal: 220,
+        fatsGoal: 73
     )
 }
--- FitnessTracker/Utilities/Constants.swift
+++ FitnessTracker/Utilities/Constants.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
+struct Constants {
+    static let edamamAppId = "901b44bd"
+    static let edamamAppKey = "1b4b28deed3ae699e7a7e339974abf81"
+}
+
 enum AppColors {
     static let primaryCTA = Color(red: 1, green: 0.84, blue: 0)       // #FFD700 Yellow
     static let calorieRing = Color(red: 1, green: 0.58, blue: 0)      // #FF9500 Orange
